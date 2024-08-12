279faacdd489b6b8611910fc32a520bdf4c89618
Gabriela Aragão <7comm777>
2024-08-12 17:36:48 -0300

    Criação inicial da Model, View e Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryMVC5.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,16 +9,58 @@ namespace LibraryMVC5.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            // Verifica as credenciais do usuário
+            if (username == "gabriela.carvalho" && password == "7777")
+            {
+                // Redireciona para a página de livros após o login bem-sucedido
+                return RedirectToAction("Books", "Books");
+            }
+
+            // Se as credenciais estiverem incorretas, retorna para a página de login com uma mensagem de erro
+            ViewBag.ErrorMessage = "Usuário ou senha incorretos.";
+            return View();
+        }
+
+        public ActionResult Books()
+        {
+            var books = new List<BookModel>
+            {
+        new BookModel { Title = "Código Limpo: Habilidades Práticas do Agile Software",
+            Category = "Engenharia de Software",
+            Author = "Robert C. Martin",
+            ISBN = "9788576082675",
+            PublicationYear = 2008,
+            IsAvailableForRental = true
+            },
+
+        new BookModel { Title = "Python para Análise de Dados: Tratamento de Dados com Pandas, NumPy e IPython",
+            Category = "Data Science",
+            Author = "Wes McKinney",
+            ISBN = "9788575225462",
+            PublicationYear = 2013,
+            IsAvailableForRental = false
+            },
+
+
+
+
+        };
+
+            return View(books);
         }
 
         public ActionResult Contact()
---
+++ Models/BookModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryMVC5.Models
+{
+    public class BookModel
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public int PublicationYear { get; set; }
+        public bool IsAvailableForRental { get; set; }
+    }
+}
