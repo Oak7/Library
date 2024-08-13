c22e482956316dca8b16554ee8c00825b3f0ce3c
Gabriela Aragão <7comm777>
2024-08-13 17:24:54 -0300

    Ajustes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,39 +35,95 @@ namespace LibraryMVC5.Controllers
             return View();
         }
 
-       
-            private List<BookModel> GetBooks()
-            {
-                return new List<BookModel>
+
+        private List<BookModel> GetBooks()
         {
-            new BookModel { Title = "Código Limpo: Habilidades Práticas do Agile Software", Category = "Engenharia de Software", Author = "Robert C. Martin", ISBN = "9788576082675", PublicationYear = 2008, IsAvailableForRental = true, ImageUrl = "codigo_limpo.jpg" },
-            new BookModel { Title = "Python para Análise de Dados", Category = "Data Science", Author = "Wes McKinney", ISBN = "9788575225462", PublicationYear = 2013, IsAvailableForRental = true, ImageUrl = "python.jpg" },
-            new BookModel { Title = "Use a Cabeça! Java", Category = "Java", Author = "Kathy Sierra, Bert Bates", ISBN = "9788576081739", PublicationYear = 2005, IsAvailableForRental = true, ImageUrl = "java.jpg" },
-            new BookModel { Title = "Aprendendo Node.js", Category = "Desenvolvimento Web", Author = "Shelly Powers", ISBN = "9788575224533", PublicationYear = 2014, IsAvailableForRental = true, ImageUrl = "node.jpg" },
-            new BookModel { Title = "Estruturas de Dados e Algoritmos com JavaScript", Category = "Algoritmos", Author = "Loiane Groner", ISBN = "9788575227183", PublicationYear = 2018, IsAvailableForRental = true, ImageUrl = "Ed.jpg" }
+            return new List<BookModel>
+        {
+            new BookModel {
+                Title = "Código Limpo: Habilidades Práticas do Agile Software",
+                Category = "Engenharia de Software", Author = "Robert C. Martin",
+                ISBN = "9788576082675",
+                PublicationYear = 2008,
+                IsAvailableForRental = true,
+                ImageUrl = "codigo_limpo.jpg" },
+
+            new BookModel {
+                Title = "Python para Análise de Dados",
+                Category = "Data Science",
+                Author = "Wes McKinney",
+                ISBN = "9788575225462",
+                PublicationYear = 2013,
+                IsAvailableForRental = true,
+                ImageUrl = "python.jpg" },
+
+            new BookModel {
+                Title = "Use a Cabeça! Java",
+                Category = "Java",
+                Author = "Kathy Sierra, Bert Bates",
+                ISBN = "9788576081739",
+                PublicationYear = 2005,
+                IsAvailableForRental = true,
+                ImageUrl = "java.jpg" },
+
+            new BookModel {
+                Title = "Aprendendo Node.js",
+                Category = "Desenvolvimento Web",
+                Author = "Shelly Powers",
+                ISBN = "9788575224533",
+                PublicationYear = 2014,
+                IsAvailableForRental = true,
+                ImageUrl = "node.jpg" },
+
+            new BookModel {
+                Title = "Estruturas de Dados e Algoritmos com JavaScript",
+                Category = "Algoritmos",
+                Author = "Loiane Groner",
+                ISBN = "9788575227183",
+                PublicationYear = 2018,
+                IsAvailableForRental = true,
+                ImageUrl = "Ed.jpg" },
+
+             new BookModel {
+                Title = "Introdução à Programação com Python",
+                Category = "Python",
+                Author = "Nilo Ney Coutinho Menezes",
+                ISBN = "9788575224144",
+                PublicationYear = 2016,
+                IsAvailableForRental = true,
+                ImageUrl = "intPython.jpg" },
+
+                new BookModel {
+                Title = " Algoritmos: Teoria e Prática",
+                Category = "Algoritmos",
+                Author = "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein",
+                ISBN = "9788535210989",
+                PublicationYear = 2009,
+                IsAvailableForRental = true,
+                ImageUrl = "algoritmos.jpg" },
         };
-            }
-
-            public ActionResult Books()
-            {
-                var books = GetBooks();
-                return View(books);
-            }
-
-            public ActionResult Detalhes(string isbn)
-            {
-                var books = GetBooks();
-                var book = books.FirstOrDefault(b => b.ISBN == isbn);
-
-                if (book == null)
-                {
-                    return HttpNotFound();
-                }
-
-                return View(book);
-            }
         }
 
+        public ActionResult Books()
+        {
+            var books = GetBooks();
+            return View(books);
+        }
 
-    
+        public ActionResult Detalhes(string isbn)
+        {
+            var books = GetBooks();
+            var book = books.FirstOrDefault(b => b.ISBN == isbn);
+
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(book);
+        }
+    }
+
+
+
 }
